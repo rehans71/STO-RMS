0af24fd35fd2cb085bcc528b10d6f105a67d56c2
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2016-04-14 20:31:58 +0530

    Initial code setup.

--- STO-RMS/src/main/java/com/sto/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sto.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-
-}
